2339cb01473bae702868e1802f6f9cd8da11aec8
perpera <131678085+perpera@users.noreply.github.com>
2023-12-06 02:21:58 +0200

    loaders add

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,18 @@ import { Component } from 'react';
 import { Searchbar } from './Searchbar';
 import { ImageGallery } from './ImageGallery';
 import toast, { Toaster } from 'react-hot-toast';
+import { Loader } from './Loader';
+import { Button } from './Button';
 
 export class App extends Component {
   state = {
     images: [],
     page: 1,
     query: '',
+    isLoading: false,
   };
 
-  componentDidUpdate(prevState) {
+  componentDidUpdate(prevProps, prevState) {
     if (
       prevState.query !== this.state.query ||
       prevState.page !== this.state.page
@@ -24,6 +27,7 @@ export class App extends Component {
     const { query, page } = this.state;
 
     try {
+      this.setState({ isLoading: true });
       const imageData = await fetchImages(query, page);
 
       if (page === 1) {
@@ -37,6 +41,8 @@ export class App extends Component {
       }
     } catch (error) {
       toast.error('Falied to fetch images. Please try again.');
+    } finally {
+      this.setState({ isLoading: false });
     }
   };
 
@@ -47,13 +53,21 @@ export class App extends Component {
     });
   };
 
+  loadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
   render() {
-    const { images } = this.state;
+    const { images, isLoading } = this.state;
 
     return (
       <div className="App">
         <Searchbar onSubmit={this.handleSubmit} />
         <ImageGallery items={images} />
+        {images.length > 0 && !isLoading && <Button onClick={this.loadMore} />}
+        {isLoading && <Loader />}
         <Toaster />
       </div>
     );
